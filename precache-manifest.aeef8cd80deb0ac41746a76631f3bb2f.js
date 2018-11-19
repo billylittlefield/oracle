5c19343620a8d302f73573addcd0f24025eb080e
Billy Littlefield <billylittlefield@gmail.com>
2018-11-19 15:22:08 -0800

    Updates

---
+++ precache-manifest.aeef8cd80deb0ac41746a76631f3bb2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7884f88b89623d1f563a",
+    "url": "/oracle/static/js/runtime~main.7884f88b.js"
+  },
+  {
+    "revision": "caca576c60249badb270",
+    "url": "/oracle/static/js/main.caca576c.chunk.js"
+  },
+  {
+    "revision": "1630119a1e92678e45df",
+    "url": "/oracle/static/js/1.1630119a.chunk.js"
+  },
+  {
+    "revision": "caca576c60249badb270",
+    "url": "/oracle/static/css/main.f63ce468.chunk.css"
+  },
+  {
+    "revision": "1f0022838597b5596e48820aad6c65d5",
+    "url": "/oracle/index.html"
+  }
+];
